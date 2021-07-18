cf8d851404fc60ff7bcc4e396ae97c76ce70f063
muhammadzubairmoosani <muhammadzubairmoosani@gmail.com>
2021-07-18 18:25:40 +0500

    monir styling

--- pages/index.js
+++ pages/index.js
@@ -2,31 +2,36 @@ import styles from "../styles/Home.module.css";
 import ReCAPTCHA from "react-google-recaptcha";
 import React, { useState } from "react";
 
+const Form = ({ token }) => (
+  <form onSubmit={(e) => e.preventDefault()}>
+    <input className={styles.input_field} required placeholder="Message..." />
+
+    <button
+      className={`${styles.send_btn} ${!token ? styles.disabled_send_btn : ""}`}
+      disabled={!token}
+    >
+      Send
+    </button>
+  </form>
+);
+
 export default function Home() {
-  const [state, setState] = useState("");
-  const [value, setValue] = useState("");
-  const [isVarified, setIsVarified] = useState(false);
+  const [token, setToken] = useState("");
 
-  function onChange(token) {
-    console.log("Captcha token:", token);
-    token && setIsVarified(true);
-  }
-
-  const _onClick = () => {
-    setValue(state);
-  };
+  const onChange = (token) => token && setToken(token);
 
   return (
     <div className={styles.container}>
-      <p>{value}</p>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input onChange={(e) => setState(e.target.value)} value={state} />
+      {token && (
+        <p className={styles.token}>
+          <b> Captcha token:</b> {token}
+        </p>
+      )}
 
-        <button disabled={!isVarified} onClick={_onClick}>
-          add value
-        </button>
-      </form>
+      <Form token={token} />
+
       <br />
+
       <ReCAPTCHA
         sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
         onChange={onChange}
