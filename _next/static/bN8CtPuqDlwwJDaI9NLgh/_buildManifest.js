d593b567cd615ac5939cdb41d142701387113eb1
muhammadzubairmoosani <muhammadzubairmoosani@gmail.com>
2021-07-18 15:39:00 +0000

    Deploying to gh-pages from @ muhammadzubairmoosani/react-google-recaptcha-example@9dfa65709c0156b68a7479de9722ee6ecf5a9d90 🚀

---
+++ _next/static/bN8CtPuqDlwwJDaI9NLgh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/208d9b1631a5f7c8159e.css","static/chunks/pages/index-83ee6f1a4450604dd206.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
